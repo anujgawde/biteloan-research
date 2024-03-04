6d9ce4adf1a486daf0b1eaf1f5849aa37fe4ebaa
ganouj01 <gawdeanuj@gmail.com>
2024-03-04 14:53:15 +0530

    environment variable changes

--- next.config.mjs
+++ next.config.mjs
@@ -5,7 +5,7 @@ const nextConfig = {
     GOOGLE_CLIENT_EMAIL: process.env.GOOGLE_CLIENT_EMAIL,
     GOOGLE_PRIVATE_KEY: process.env.GOOGLE_PRIVATE_KEY,
     GOOGLE_SHEET_ID: process.env.GOOGLE_SHEET_ID,
-    GAID: process.env.GAID,
+    GOOGLE_ANALYTICS: process.env.GOOGLE_ANALYTICS,
   },
 };
 
